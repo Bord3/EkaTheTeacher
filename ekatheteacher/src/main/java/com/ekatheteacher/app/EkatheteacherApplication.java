60cc799d579b8466057e1081cef7b439c20e229a
Dani_ <Dani_@DESKTOP-DBFH9EB>
2019-09-27 15:15:05 +0200

    changed name

--- ekatheteacher/src/main/java/com/ekatheteacher/app/EkatheteacherApplication.java
+++ ekatheteacher/src/main/java/com/ekatheteacher/app/EkatheteacherApplication.java
@@ -1,4 +1,4 @@
-package com.ekatheteacher;
+package com.ekatheteacher.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
