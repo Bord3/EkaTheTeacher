e9ecf402fa934b644eb74c9d96d20aa54c9f41f4
Dani_ <Dani_@DESKTOP-DBFH9EB>
2019-09-27 14:49:08 +0200

    origin

---
+++ ekatheteacher/src/main/java/com/ekatheteacher/EkatheteacherApplication.java
@@ -0,0 +1,13 @@
+package com.ekatheteacher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EkatheteacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EkatheteacherApplication.class, args);
+	}
+
+}
